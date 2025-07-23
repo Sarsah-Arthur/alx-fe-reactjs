4cd9ac9d444d47b80efe0fbbee2febb868fa2346
Sarsah-Arthur <kobinadentwi@gmail.com>
2025-07-23 22:57:13 +0000

    Added Advanced Search & Filters

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,31 @@
 import { useRecipeStore } from '../store/recipeStore';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
+  const filterRecipes = useRecipeStore((state) => state.filterRecipes);
+
+  useEffect(() => {
+    filterRecipes();
+  }, [searchTerm, filterRecipes]);
 
   return (
     <div>
       <h2>Recipes</h2>
       {filteredRecipes.map((recipe) => (
-        <div key={recipe.id} style={{ border: '1px solid #ccc', marginBottom: '10px', padding: '10px' }}>
-          <h3>{recipe.title}</h3>
+        <div
+          key={recipe.id}
+          style={{
+            border: '1px solid #ccc',
+            marginBottom: '10px',
+            padding: '10px',
+          }}
+        >
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
